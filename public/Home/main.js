3a0afcb4adb3c31af8420280e89f5ee90dfa7e8d
JP1005YT <godlolpro32@gmail.com>
2023-10-27 19:49:41 -0300

    "Pronto"

--- public/Home/main.js
+++ public/Home/main.js
@@ -20,6 +20,7 @@ async function Check(){
     resposta = await dados.json();
     if(resposta != false){
         userdata = resposta
+        console.log(userdata)
        document.querySelector("#user").innerHTML = userdata.name 
     }else{
         window.location.href = '../'
@@ -31,7 +32,37 @@ Check()
 addEventListener("keydown",function(e){
     if(e.key === "Enter"){
         let input = document.querySelector(".chatInput")
-        input.value
-        socket.emit("chat",Id_Chat,message);
+        if(input.value.length > 0){
+            let message = {
+                "msg" : input.value,
+                "user" : userdata.name
+            }
+            input.value = ''
+            socket.emit("chat",message);
+        }
+        // console.log(message)
     }
 })
+socket.on("chatreceive",(msg)=>{
+    ConstruirMSG(msg)
+})
+let chat = document.getElementById("chatGlobal");
+function ConstruirMSG(msg){
+    if(msg.user === userdata.name){
+        let div = document.createElement("div");
+        div.classList.value = 'message my';
+        msg.hora = msg.hora.split(" ");
+        div.innerHTML = `<div class="content"><span class="name">${msg.user}</span><span class="text">${msg.msg}</span><span class="time">${msg.hora[1]}</span></div>`;
+        chat.appendChild(div);
+        document.documentElement.scrollTop  = chat.scrollHeight
+    }else{
+        let div = document.createElement("div");
+        div.classList.value = 'message';
+        msg.hora = msg.hora.split(" ");
+        div.innerHTML = `<div class="content"><span class="name">${msg.user}</span><span class="text">${msg.msg}</span><span class="time">${msg.hora[1]}</span></div>`;
+        chat.appendChild(div);
+        document.documentElement.scrollTop  = chat.scrollHeight
+    }
+
+}
+
--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ let P = new Pacotes()
 const {Utils} = require("../components/utils.js");
 let U = new Utils()
 const db = require("../configs/Bd.js")
-
+let io = S.io
 S.start()
 
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado');
+
+    socket.on('chat',( message) => {
+        const dataAgora = new Date()
+        message.hora = formatarData(dataAgora)
+        io.emit('chatreceive',message)
+    });
+})
 const app = S.app
 // Banco de dados
 app.post("/login",async (req,res) => {
@@ -38,13 +47,22 @@ app.post("/login",async (req,res) => {
 
 app.post("/check",async (req,res) => {
     const data = await P.Buscar("./data/users.json")
-    let passa
+    let passa = false
     data.users.forEach(user => {
+        console.log(user)
         if(user.id === req.body.id){
             passa =  user
-        }else{
-            passa =  false
         }
     });
     res.send(passa)
 })
+
+function formatarData(data) {
+    const dia = String(data.getDate()).padStart(2, '0');
+    const mes = String(data.getMonth() + 1).padStart(2, '0');
+    const ano = String(data.getFullYear()).slice(-2);
+    const hora = String(data.getHours()).padStart(2, '0');
+    const minutos = String(data.getMinutes()).padStart(2, '0');
+
+    return `${dia}/${mes}/${ano} ${hora}:${minutos}`;
+}
